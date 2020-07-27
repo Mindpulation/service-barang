b05d129d10b6907272605dbe6a791f1676ccea05
Indra <me@aboutindra.com>
2020-07-27 21:06:34 +0700

    Update data.go

---
+++ data/data.go
@@ -0,0 +1,33 @@
+package data
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type ParamIdBarang struct {
+	Id primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+}
+
+type DataUlasan struct {
+	Nama   string
+	Ulasan string
+	Rating float32
+}
+
+type DataStok struct {
+	Stok uint64
+}
+
+type DataBarang struct {
+	Id           primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	IdToko       string
+	Nama         string
+	Deskripsi    string
+	TotalUlasan  uint64
+	TotalTerjual uint64
+	Keyword      []string
+	Rating       uint64
+	Price        uint64
+	Stok         uint32
+	Ulasan       []DataUlasan
+}
